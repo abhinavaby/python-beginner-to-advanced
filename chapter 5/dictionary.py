ae7e7e3c98ca9d7ca4a52e2b43077756ed079283
abhinavaby <abhinavaby07@gmail.com>
2026-05-16 16:32:48 +0000

    A70410:02 PM136-48PMve [${brPMn610}] | ${16PM10:02 PMe} PM10:02 PM ${10:02 PMi2e}

--- chapter 5/dictionary.py
+++ chapter 5/dictionary.py
@@ -1,8 +1,9 @@
-mark={
-    "abhinav":10,
-    "abel":23,
-    "jacob":55
+mark = {
+    "abhinav": 10,
+    "abel": 23,
+    "jacob": 55
 }
-print(mark,type(mark))
-print(f"mark of abhinav:{mark["abhinav"]}")
-print(f"mark of abel: {mark["abel"]}")
+
+print(mark, type(mark))
+print(f"mark of abhinav: {mark['abhinav']}")
+print(f"mark of abel: {mark['abel']}")
