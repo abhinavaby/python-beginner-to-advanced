9b13a1200c2e21ce184bc114cbe8d701427458ab
abhinavaby <abhinavaby07@gmail.com>
2026-05-21 09:04:23 +0000

    A3932:34 PM141-23PMve [${brPMn42}] | ${21PM2:34 PMe} PM2:34 PM ${2:34 PMi34e}

--- chapter 6/conditional-statement.py
+++ chapter 6/conditional-statement.py
@@ -1,7 +1,24 @@
 # if anycondition is met da a certain thing 
 
+
+#if else
+
 a=int(input("enter the age: "))
-if a > 18:
+if a >= 18:
     print("you are eligibe for voting......")
 else:
-    print("you cant vote..")
+    print("you cant vote.........")
+print("end of program")
+
+
+#if else elif
+
+a=int(input("enter the age: "))
+if a >= 18:
+    print("you are eligibe for voting......")
+elif(a>100 and a<0):
+    print("you entered an invalid age......")
+else:
+    print("you cant vote.........")
+print("end of program")
+
