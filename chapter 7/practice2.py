c5044087124e628ba15770ceadfb7a17a48cfdca
abhinavaby <abhinavaby07@gmail.com>
2026-05-21 13:52:24 +0000

    414122i7:22 PM

---
+++ chapter 7/practice2.py
@@ -0,0 +1,5 @@
+#2. Write a program to greet all the person names stored in a list ‘l’ and which starts with S.
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+for i in l:
+    if(i[0]=="S"):
+        print(f"Hello {i}")
