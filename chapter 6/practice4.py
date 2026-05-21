19de7e87ce191bbe6a71464fe971cf5bb57d728e
abhinavaby <abhinavaby07@gmail.com>
2026-05-21 09:51:41 +0000

    414121i3:21 PM

---
+++ chapter 6/practice4.py
@@ -0,0 +1,6 @@
+#4. Write a program to find whether a given username contains less than 10 characters or not.
+w=input("enter the user name: ")
+if(len(w)<10):
+    print("user name contain less than 10 charecters")
+else:
+    print("user contain more than 10 cahrecters")
