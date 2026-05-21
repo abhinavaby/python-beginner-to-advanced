950b0f77061cf08473cf7a6dbacbaeb9c882cc9b
abhinavaby <abhinavaby07@gmail.com>
2026-05-21 13:50:18 +0000

    414120i7:20 PM

--- chapter 7/practice1.py
+++ chapter 7/practice1.py
@@ -1,4 +1,4 @@
 #1. Write a program to print multiplication table of a given number using for loop.
-w=input("enter the number: ")
+w=int(input("enter the number: "))
 for i in range(1,11):
     print(f"{i} * {w} = {i*w}")
