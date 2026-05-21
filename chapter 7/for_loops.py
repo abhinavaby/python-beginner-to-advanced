8ca3e77f40335c867ad863cbfcd1787d746d19a1
abhinavaby <abhinavaby07@gmail.com>
2026-05-21 11:14:39 +0000

    414144i4:44 PM

--- chapter 7/for_loops.py
+++ chapter 7/for_loops.py
@@ -4,6 +4,11 @@ for i in range(1,11):
 print()
 print()
 
-# printing 1 to 20 with step 4
-for i in range(2,21,2):
+# printing 1 to 20 with step 2
+for i in range(1,21,2): # prints all odd numbers
+    print(i)
+
+print()
+# printing 1 to 20 with step 2
+for i in range(2,21,2): # prints all even numbers
     print(i)
