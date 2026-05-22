e910879acb99e6bd3e347950b86dc35ce9f5ca5d
abhinavaby <abhinavaby07@gmail.com>
2026-05-22 07:55:36 +0000

    514225i1:25 PM

--- chapter 7/practice10.py
+++ chapter 7/practice10.py
@@ -1,4 +1,4 @@
 #10. Write a program to print multiplication table of n using for loops in reversed order.
 w=int(input("enter the number: "))
-for i in range(10,1):
+for i in range(10,1,-1):
     print(f"{i} * {w} = {i*w}")
