fbd315615fb3defbcbeb888d9c3d6bb516b598c8
abhinavaby <abhinavaby07@gmail.com>
2026-05-22 08:13:08 +0000

    514243i1:43 PM

--- chapter 7/practice7.py
+++ chapter 7/practice7.py
@@ -3,3 +3,12 @@
  ***
 ***** for n = 3
 """
+# Input the number of rows
+n = int(input("Enter the number of rows (n): "))
+
+# Loop for each row
+for i in range(1, n + 1):
+    # Print spaces before the stars
+    print(" " * (n - i), end="")
+    # Print the stars for the current row
+    print("*" * (2 * i - 1))
