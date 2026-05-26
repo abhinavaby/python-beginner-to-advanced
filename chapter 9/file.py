6d2b9265f81a97232664ef4af302bac3099cb2d0
abhinavaby <abhinavaby07@gmail.com>
2026-05-26 22:20:49 +0530

    commit

---
+++ chapter 9/file.py
@@ -0,0 +1,9 @@
+# saves the data in HDD rather than vanishing when the program is over
+# text file:txt,py,c,etc
+#binary file:jpg,dat,etc
+
+#reading a file
+f= open("first.txt")
+data=f.read()
+print(data)
+f.close()
